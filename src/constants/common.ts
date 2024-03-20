be1b97b5962baeae5094c30332acc90d8fc03ebd
ThangDevAlone <quangthangvtlg@gmail.com>
2024-03-20 10:50:19 +0700

    dád

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,7 +1,7 @@
 // export const STATIC_HOST = 'https://hrmbe.onrender.com/';
 
-// export const STATIC_HOST = 'https://api-hrm.whiteneurons.com/';
-// export const STATIC_HOST_NO_SPLASH = 'https://api-hrm.whiteneurons.com';
+export const STATIC_HOST = 'https://api-hrm.whiteneurons.com/';
+export const STATIC_HOST_NO_SPLASH = 'https://api-hrm.whiteneurons.com';
 
-export const STATIC_HOST = 'http://127.0.0.1:8000/';
-export const STATIC_HOST_NO_SPLASH = 'http://127.0.0.1:8000';
+// export const STATIC_HOST = 'http://127.0.0.1:8000/';
+// export const STATIC_HOST_NO_SPLASH = 'http://127.0.0.1:8000';
